de097739cd4a8fe879a30678f0f35a15ec500678
lzakrzewski <lucaszz111@gmail.com>
2016-05-15 16:42:34 +0200

    Doctrine Database Backup introduced

---
+++ tests/integration/DatabaseTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\integration;
+
+abstract class DatabaseTestCase extends IntegrationTestCase
+{
+    /** {@inheritdoc} */
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->container()
+            ->get('es_sandbox.test.doctrine_database_backup')
+            ->restore();
+    }
+}
